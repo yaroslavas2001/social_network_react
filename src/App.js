0d620d4bb23dd2e1abe82db8263cdfce0eae2c87
yaroslavas2001 <61469500+yaroslavas2001@users.noreply.github.com>
2022-09-05 16:08:58 +0300

    компонента

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.12345678
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Technologgies />
     </div>
   );
 }
-
+const Technologgies = () => {
+  return (<div>
+    123456789
+  </div>);
+}
+const Header = () => {
+  return (
+    <div>
+      <li>2</li>
+      <li>3</li>
+      <li>4</li>
+    </div>
+  );
+}
 export default App;
