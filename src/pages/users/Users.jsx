dca919fec0dc2d420b9e006680933356f6615495
yaroslavas2001 <61469500+yaroslavas2001@users.noreply.github.com>
2022-10-16 19:17:42 +0300

    axios

--- src/pages/users/Users.jsx
+++ src/pages/users/Users.jsx
@@ -1,40 +1,18 @@
+import axios from "axios"
 import s from "./Users.module.css"
 const Users = (props) => {
   console.log("users", props.users)
   // if()
-  if (props.users.length == 0) props.setUsers([
-    {
-      id: 2, fullName: "Yaroslava", status: 'Im looking for a job',
-      photoUrl: "https://www.w3schools.com/howto/img_avatar2.png",
-      followed: true,
-      location: {
-        city: "Tiraspol",
-        countru: "Moldova"
-      }
-    },
-    {
-      id: 3, fullName: "Slava", status: 'Im looking for a job',
-      followed: false,
-      photoUrl: "https://www.w3schools.com/howto/img_avatar2.png",
-      location: {
-        city: "Bender",
-        countru: "Moldova"
-      }
-    },
-    {
-      id: 4, fullName: "Kolya", status: 'Im looking for a job',
-      followed: true,
-      photoUrl: "https://www.w3schools.com/howto/img_avatar2.png",
-      location: {
-        city: "Keiv",
-        countru: "Ukrain"
-      }
-    },])
+ 
+  if (props.users.length == 0)  axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+    props.setUsers(response.data.items)
+    // console.log("data", data)
+  })
   return (<div className="test">
     {
       props.users.map(el => <div key={el.id}>
-        <div>{el.fullName}</div>
-        <img className={s.photo} src={el.photoUrl} alt="" />
+        <div>{el.name}</div>
+        <img className={s.photo} src={el.photos.large?el.photos.large:"https://www.w3schools.com/howto/img_avatar2.png"} alt="" />
         {el.followed ?
           <button className={[s.btn, s.unfollow].join(' ')} onClick={() => { props.unFollow(el.id) }}>UnFollow</button> :
           <button className={[s.btn, s.follow].join(' ')} onClick={() => { props.follow(el.id) }}>Follow</button>}
