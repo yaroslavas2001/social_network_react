39ab418cfa5e9f955ae365456b9736dea3df2bf7
yaroslavas2001 <61469500+yaroslavas2001@users.noreply.github.com>
2022-11-14 20:00:04 +0200

    расширения

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -1,21 +1,24 @@
-import { applyMiddleware, combineReducers, legacy_createStore as createStore } from "redux";
+import { applyMiddleware, combineReducers, legacy_createStore as createStore, compose } from "redux";
 import profileReducer from "./profile-reducer"
 import dialogsReducer from "./dialogs-reducer"
 import usersReducer from "./users-reducer";
 import authReducer from "./auth-reducer";
-import thunk   from "redux-thunk"
+import thunk from "redux-thunk"
 import appReduce from "./app-reducer"
 import { reducer as formReducer } from 'redux-form'
+
+
 let reducers = combineReducers({
   profilePage: profileReducer,
   dialogsPage: dialogsReducer,
   userPage: usersReducer,
-  auth:authReducer,
-  app:appReduce,
-  form:formReducer
+  auth: authReducer,
+  app: appReduce,
+  form: formReducer
 })
-
-let store = createStore(reducers,applyMiddleware(thunk))
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
+// let store = createStore(reducers,applyMiddleware(thunk))
 
 
 export default store
